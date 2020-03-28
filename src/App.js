baea2e2d70704efbad1975e6efcd2543ab408943
Fatih Kucuk <1fatihkucuk@gmail.com>
2020-03-28 19:44:58 +0300

    Fix router by using hash router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from './components/NavigationBar/NavigationBar';
-import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { Switch, Route, HashRouter, Redirect } from 'react-router-dom';
 import routes from './routes';
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <NavigationBar />
       <Switch>
-        <Redirect exact from="/" to="/item-list" />
+        <Redirect exact from="/" to="item-list" />
         {routes.map((route) => {
           return <Route key={route.path} exact {...route} />;
         })}
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
